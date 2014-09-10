dfe82e4d545ff975ddd3d7b7f783c6901f600d64
posenhuang <huang.person@gmail.com>
2014-09-09 22:57:06 -0500

    add run_test_single_model to timit

---
+++ codes/timit/run_test_single_model.m
@@ -0,0 +1,31 @@
+function run_test_single_model
+% (TODO) add
+% Given a model, evaluate the performance.
+    baseDir = '../../';
+    addpath([baseDir, filesep, 'codes']);
+    addpath([baseDir, filesep, 'tools', filesep,'bss_eval']);
+    addpath([baseDir, filesep, 'tools', filesep,'bss_eval_3']);
+    addpath([baseDir, filesep, 'tools', filesep,'labrosa']);
+    addpath(['Data_with_dev']);
+
+    ModelPath=[baseDir, filesep, 'codes',filesep, 'timit', filesep, 'model_demo'];
+
+    global SDR;
+
+    SDR.deviter=0;   SDR.devmax=0;   SDR.testmax=0;
+    SDR.devsar=0; SDR.devsir=0; SDR.testsar=0; SDR.testsir=0;
+
+    j=10;
+
+    % Load model
+    load([ModelPath, filesep, 'model_', num2str(j),'.mat']);
+    eI.writewav=1;
+    eI.bss3=1;
+    eI.DataPath=[baseDir, filesep, 'codes', filesep, 'timit', ...
+        filesep, 'Wavfile', filesep];
+    eI.saveDir = [baseDir, filesep, 'codes', filesep, 'timit', ...
+        filesep, 'model_demo', filesep, 'results', filesep];
+  
+    test_timit_general_kl_recurrent(eI.modelname, theta, eI, 'testall', info.iteration);
+end
+
